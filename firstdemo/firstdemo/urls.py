9c65506420445f84e6fc30da3feff02c69d34b86
docostar <dholariyarahul@gmail.com>
2024-02-12 11:25:45 +0000

    multiple views created and configure urls

--- firstdemo/firstdemo/urls.py
+++ firstdemo/firstdemo/urls.py
@@ -20,5 +20,6 @@ from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',views.display)
+    path('hello/',views.display),
+    path('date/',views.displayDateTime)
 ]
--- firstdemo/home/views.py
+++ firstdemo/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 def display(request):
     return HttpResponse("<h1>This is Django ITI App.</h1>")
+
+
+def displayDateTime(request):
+    dt=datetime.datetime.now()
+    s="<b>Curren time is:</b>"+ str(dt)
+    return HttpResponse(s)
