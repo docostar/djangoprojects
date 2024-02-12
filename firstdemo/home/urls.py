0de091458a4eb563620031042b50ce766155e6be
docostar <dholariyarahul@gmail.com>
2024-02-12 11:50:08 +0000

    App level Urls created

--- firstdemo/home/urls.py
+++ firstdemo/home/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
-
+from home import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/',include('home.urls')),
-    path('quoteapp/',include('quoteapp.urls'))
+    path('hello/',views.display),
+    path('date/',views.displayDateTime),
 ]
