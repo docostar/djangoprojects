41b714830dcb22ff4e4fb62aeefb2325919bbebc
docostar <dholariyarahul@gmail.com>
2024-03-08 06:25:22 +0000

    Retrive Database Data and Display in Templates

--- modeldemo/empApp/views.py
+++ modeldemo/empApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from empApp.models import Employee
 
 # Create your views here.
+def employeeData(request):
+    empls=Employee.objects.all()
+    empDict={"employees":empls}
+    return render(request,"empApp/employee.html",empDict)
--- modeldemo/modeldemo/settings.py
+++ modeldemo/modeldemo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!m-_ctg)w@v+##r=ygf_j=efq%kpz4rbka5a%$7auw%&974oo@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-docostar-djangoprojects-jzk2u2oudf3.ws-us108.gitpod.io']
 
 
 # Application definition
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'modeldemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
