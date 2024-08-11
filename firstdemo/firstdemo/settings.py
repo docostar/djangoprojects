655b0b97ca21198869d171ca57954e4247314762
docostar <dholariyarahul@gmail.com>
2024-08-11 05:08:02 +0000

    Allowed Host change in firstdemo setting

--- firstdemo/firstdemo/settings.py
+++ firstdemo/firstdemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!s=o3z6gz_(n)a=ocqz2pxpz^9!+sn+dcp=udwz92=s!@-b8vv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '8000-docostar-djangoprojects-jzk2u2oudf3.ws-us108.gitpod.io',
+ALLOWED_HOSTS = [ '8000-docostar-djangoprojects-qdemxcr20lp.ws-us115.gitpod.io',
     '8002-docostar-djangoprojects-jzk2u2oudf3.ws-us108.gitpod.io']
 
 
